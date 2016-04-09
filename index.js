0142f28385978ac1f1fb95f0821bc676259cedf1
Zhandos Nurzhanov <zirun741@gmail.com>
2016-04-10 05:22:23 +0600

    dss

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var distance = 0;
 var crawlerHead = gpio.export(13, { // PIN 33
    direction: "in",
    ready: function() {
-     crawlerHead.set();
+     crawlerHead.set(1);
      console.log('ready', crawlerHead.value);
    }
 });
@@ -27,7 +27,7 @@ var crawlerHead = gpio.export(13, { // PIN 33
 var crawlerTail = gpio.export(19, { // PIN 35
    direction: "in",
    ready: function() {
-     crawlerTail.set();
+     crawlerTail.set(1);
      console.log('ready', crawlerTail.value);
    }
 });
