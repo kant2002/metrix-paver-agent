9879eafe482c42065cbe8db89e96a1fa5334cf24
Zhandos Nurzhanov <zirun741@gmail.com>
2016-04-10 02:53:56 +0600

    pdf

--- index.js
+++ index.js
@@ -34,17 +34,35 @@ crawlerHead.on("change", function(val) {
    // value will report either 1 or 0 (number) when the value changes
 
    if(val == 1){
-     if(cHead == 0) cHead = 1;
-     else {
-       if(cHead == 1){
-         console.log('step stop backward | ', crawlerTail.value);
-       }
-       else {
-         console.log('step backward      | ', crawlerTail.value);
-       }
-      cHead = 0;
+     if(crawlerTail.value == 0){
+       console.log('Forward');
+     }
+     else{
+       console.log('Reverse');
      }
    }
+   else {
+     if(crawlerTail.value == 0){
+       console.log('Reverse');
+     }
+     else{
+       console.log('Forward');
+     }
+   }
+
+
+  //  if(val == 1){
+  //    if(cHead == 0) cHead = 1;
+  //    else {
+  //      if(cHead == 1){
+  //        console.log('step stop backward | ', crawlerTail.value);
+  //      }
+  //      else {
+  //        console.log('step backward      | ', crawlerTail.value);
+  //      }
+  //     cHead = 0;
+  //    }
+  //  }
 
 
 
@@ -53,18 +71,35 @@ crawlerHead.on("change", function(val) {
 crawlerTail.on("change", function(val) {
    // value will report either 1 or 0 (number) when the value changes
 
-   if(val == 1){
-     if(cHead == 0) cHead = 2;
-     else {
-       if(cHead == 2){
-         console.log('step stop forward  | ', crawlerHead.value);
-       }
-       else {
-         console.log('step forward       | ', crawlerHead.value); 
-       }
-       cHead = 0;
+   if(val == 0){
+     if(crawlerHead.value == 0){
+       console.log('Forward');
+     }
+     else{
+       console.log('Reverse');
      }
    }
+   else {
+     if(crawlerHead.value == 0){
+       console.log('Reverse');
+     }
+     else{
+       console.log('Forward');
+     }
+   }
+
+  //  if(val == 1){
+  //    if(cHead == 0) cHead = 2;
+  //    else {
+  //      if(cHead == 2){
+  //        console.log('step stop forward  | ', crawlerHead.value);
+  //      }
+  //      else {
+  //        console.log('step forward       | ', crawlerHead.value);
+  //      }
+  //      cHead = 0;
+  //    }
+  //  }
 
 
 
