30d6bb7df640e352a90224718bac27e6573e1565
Zhandos Nurzhanov <zirun741@gmail.com>
2016-04-15 04:45:25 +0600

    df

--- encoder.py
+++ encoder.py
@@ -11,59 +11,47 @@ GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 distance=0
-head = 0;
-tail = 0;
 
-def head_rise(channel):
+def head_gpio(channel):
     global distance
-    global head
-    head = 1
-    if tail == 0:
-        distance-=1
-        print distance
+    if GPIO.input(13):
+        if GPIO.input(19) == 0:
+            distance+=1
+            print distance
+        else:
+            distance-=1
+            print distance
     else:
-        distance+=1
-        print distance
+        if GPIO.input(19) == 0:
+            distance-=1
+            print distance
+        else:
+            distance+=1
+            print distance
 
-def head_fall(channel):
+def tail_gpio(channel):
     global distance
-    global head
-    head = 0
-    if tail == 0:
-        distance+=1
-        print distance
+    if GPIO.input(19):
+        if GPIO.input(13) == 0:
+            distance-=1
+            print distance
+        else:
+            distance+=1
+            print distance
     else:
-        distance-=1
-        print distance
+        if GPIO.input(13) == 0:
+            distance+=1
+            print distance
+        else:
+            distance-=1
+            print distance
 
-def tail_rise(channel):
-    global distance
-    global tail
-    tail = 1
-    if head == 0:
-        distance+=1
-        print distance
-    else:
-        distance-=1
-        print distance
-def tail_fall(channel):
-    global distance
-    global tail
-    tail = 0
-    if head == 0:
-        distance-=1
-        print distance
-    else:
-        distance+=1
-        print distance
 
 
 # when a falling edge is detected on port 17, regardless of whatever
 # else is happening in the program, the function my_callback will be run
-GPIO.add_event_detect(13, GPIO.RISING, callback=head_rise)
-GPIO.add_event_detect(13, GPIO.FALLING, callback=head_fall)
-GPIO.add_event_detect(19, GPIO.RISING, callback=tail_rise)
-GPIO.add_event_detect(19, GPIO.FALLING, callback=tail_fall)
+GPIO.add_event_detect(13, GPIO.BOTH, callback=head_gpio)
+GPIO.add_event_detect(19, GPIO.BOTH, callback=tail_gpio)
 
 # when a falling edge is detected on port 23, regardless of whatever
 # else is happening in the program, the function my_callback2 will be run
