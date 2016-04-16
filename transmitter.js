315f42d9faf59acccbff0bf14ef473f8af70a9e4
Zhandos Nurzhanov <zirun741@gmail.com>
2016-04-16 06:10:34 +0600

    qq

--- transmitter.js
+++ transmitter.js
@@ -19,7 +19,7 @@ Transmitter.prototype.sync = function(data){
     return 0;
   }
 
-  this.getScope.then(function(scope){
+  this.getScope().then(function(scope){
     self.DB.query('SELECT * from `paverTrace` WHERE `finishTime` > ?', [scope.updatedAt], function(err, rows) {
       if(err || rows.length == 0){
         console.log('err:', err);
