764c0930949a6afea1e30871cfc683fe6e764fda
Zhandos Nurzhanov <zirun741@gmail.com>
2016-04-18 14:26:33 +0600

    sd

--- index.js
+++ index.js
@@ -33,46 +33,6 @@ DB.connect(function(err) {
   transmitter.sync();
 })
 
-
-
-
-// var port = new serialport.SerialPort('/dev/ttyACM0', {
-//                 baudrate: 9600,
-//                 parser: serialport.parsers.readline('\r\n')});
-//
-// var nmea_codes = ['GGA', 'GLL'];
-//
-// var nmea_codes_arr = [];
-//
-// port.on('data', function(line) {
-//   try {
-//     // console.log('NMEA:', nmea.parse(line));
-//     var gis = nmea.parse(line);
-//
-//     if(nmea_codes_arr.indexOf(gis.sentence) == -1){
-//       nmea_codes_arr.push(gis.sentence);
-//     }
-//
-//     if(nmea_codes.indexOf(gis.sentence) > -1){
-//       console.log(gis.sentence, '--------------------------');
-//       console.log('lat:', gis.lat);
-//       console.log('lon:', gis.lon);
-//       if(gis.type) console.log('type:', gis.type);
-//       if(gis.fixType) console.log('fix-type:', gis.fixType);
-//       if(gis.status) console.log('status', gis.status);0
-//     }
-//
-//
-//   } catch (e) {
-//       console.log('err', e);
-//   }
-// });
-//
-// setInterval(function(){
-//   console.log('NMEA CODES: ', nmea_codes_arr);
-//
-// },5000);
-
 var cRadius = 0.22765;
 var crawlerSpace = 10; //constant
 var positionFault = true;
@@ -87,6 +47,26 @@ var record = {
 };
 var dowelCurrent = 0;
 
+
+
+var GPS_port = new serialport.SerialPort('/dev/ttyACM0', {
+                baudrate: 9600,
+                parser: serialport.parsers.readline('\r\n')});
+
+var nmea_codes = ['GGA', 'GLL'];
+
+GPS_port.on('data', function(line) {
+  try {
+    var gis = nmea.parse(line);
+    if(nmea_codes.indexOf(gis.sentence) > -1){
+      console.log(gis.sentence, '--------------------------');
+      record.latitude = gis.lat;
+      record.longitude =  gis.lon;
+    }
+  } catch (e) {
+      console.log('err', e);
+  }
+});
 
 var dowelExist = gpio.export(26, { // PIN 27
    direction: "in",
