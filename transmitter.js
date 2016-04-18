5de9d1250e8b049052bc61d4baa7c4d096b99476
Zhandos Nurzhanov <zirun741@gmail.com>
2016-04-18 12:03:37 +0600

    sd

--- transmitter.js
+++ transmitter.js
@@ -29,8 +29,7 @@ Transmitter.prototype.sync = function(data){
   }
 
   axios.get(self.host + 'api/production/paverIndex/?deviceId=' + self.deviceId, {}).then(function(response){
-    self.DB.query('SELECT * from `paverTrace` WHERE `finishTime` > ?', [response.data.updatedAt], function(err, rows) {
-      console.log('rows:', rows);
+    self.DB.query('SELECT * from `paverTrace` WHERE `finishTime` > ?', [response.data.updatedAt.toLocaleString('en-US', { timeZone: 'Asia/Almaty' })], function(err, rows) {
       if(err || rows.length == 0){
         console.log('No Data Left:', err);
         self.lastTransmission = new Date();
