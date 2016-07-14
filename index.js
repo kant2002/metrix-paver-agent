e66195b305a2dae4e91b7c16d5cb366266c937d5
Zhandos Nurzhanov <zirun741@gmail.com>
2016-07-14 11:31:20 +0600

    encoder absolute fix

--- index.js
+++ index.js
@@ -231,7 +231,7 @@ function initialize(){
       muteSignal('dowelDip');
       dowelRecord.finishTime = moment().utc().format();
       redisCli.get('dist', function(err, reply){
-        dowelRecord.distance = parseInt(reply)*WHEEL_R;
+        dowelRecord.distance = Math.round((Math.abs(parseInt(reply)*WHEEL_R, )*100))/100;
         if(dowelRecord.distance > 1){
           DB.query('INSERT INTO setPoint SET ?', dowelRecord, function(err, rows){
             if(err){
@@ -292,7 +292,7 @@ function initialize(){
       muteSignal('tieDip');
       setTimeout(tieCheck, 11000);
       redisCli.get('tiedist', function(err, reply){
-        tieRecord.distance = parseInt(reply)*WHEEL_R;
+        tieRecord.distance = Math.round((Math.abs(parseInt(reply)*WHEEL_R, )*100))/100;
         if(tieRecord.distance > 0.2){
           DB.query('INSERT INTO tiePoint SET ?', tieRecord, function(err, rows){
             if(err){
